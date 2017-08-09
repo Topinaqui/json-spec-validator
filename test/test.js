0a717316422f8418735bef71a20c20119fa23452
Franklin Oliveira da Veiga <aionprofile@hotmail.com>
2017-08-09 06:15:34 -0300

    Initial release

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('json-spec-validator', function() {
+    it('should validate a json based on a given spec', function() {
+
+        //
+    });
+});
